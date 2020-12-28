5bf38a07bb7f316036bfcf030af7f3f62aa4a9c9
S. Morad <sm2558@cam.ac.uk>
2020-12-28 19:00:22 +0000

    Ray+habitat trains without crashing

--- src/rayenv.py
+++ src/rayenv.py
@@ -1,6 +1,7 @@
 import habitat
 import gym, ray
 from ray.rllib.agents import ppo
+from gym.spaces import discrete
 
 
 class NavEnv(habitat.RLEnv):
@@ -20,10 +21,21 @@ class NavEnv(habitat.RLEnv):
     def close(self):
         return super().close() 
     '''
+    # Habitat override
 
     def __init__(self, cfg):
         hab_cfg = habitat.get_config(config_paths=cfg['path'])
-        return super().__init__(hab_cfg)
+        rv = super().__init__(hab_cfg)
+
+        # Patch action space since habitat actions use custom spaces for some reason
+        # TODO: these should translate for continuous/arbitrary action distribution
+        # Order: forward, stop, left, right
+        self.action_space = discrete.Discrete(4)
+
+    def action_space(self):
+        # TODO: these should translate for continuous/arbitrary action distribution
+        # Order: forward, stop, left, right
+        return discrete.Discrete(4)
 
     # Habitat iface that we impl
     def get_reward_range(self):
@@ -43,9 +55,13 @@ if __name__ == '__main__':
     ray.init()
     #hab_cfg = habitat.get_config(config_paths="/root/habitat-lab/configs/tasks/pointnav.yaml")
     hab_cfg_path = "/root/habitat-lab/configs/tasks/pointnav.yaml"
+    '''
+    hab = NavEnv(cfg={'path': hab_cfg_path})
+    import pdb; pdb.set_trace()
+    '''
     ray_cfg = {'env_config': {'path': hab_cfg_path}, 
-            'num_workers': 2,
-            'num_gpus_per_worker': 2,
+            'num_workers': 0,
+            #'num_gpus_per_worker': 0.5,
             'framework': 'torch'}
     trainer = ppo.PPOTrainer(env=NavEnv, config=ray_cfg)
     while True:
