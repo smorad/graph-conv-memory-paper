5eb519f64ff9eaa05a4916a3e80735cf0fd88e47
S. Morad <sm2558@cam.ac.uk>
2021-01-18 16:35:52 +0000

    Add files before destroying container

---
+++ src/models/gnn.py
@@ -0,0 +1,27 @@
+import torch
+from torch.nn import Sequential, Linear, ReLU
+from torch_geometric.nn import MessagePassing
+
+
+class EgoGNNModel(MessagePassing):
+    """
+    The model takes as input a euclidean graph. Each node contains a pose relative to
+    the current pose and the observation from said pose.
+
+    The output of the model is a distribution for a continuous action (angle, magnitude).
+    """
+
+    def __init__(self, F_in, F_out):
+        super(EdgeConv, self).__init__(aggr='max')  # "Max" aggregation.
+        self.mlp = Sequential(Linear(2 * F_in, F_out), ReLU(), Linear(F_out, F_out))
+
+    def forward(self, x, edge_index):
+        # x has shape [N, F_in]
+        # edge_index has shape [2, E]
+        return self.propagate(edge_index, x=x)  # shape [N, F_out]
+
+    def message(self, x_i, x_j):
+        # x_i has shape [E, F_in]
+        # x_j has shape [E, F_in]
+        edge_features = torch.cat([x_i, x_j - x_i], dim=1)  # shape [E, 2 * F_in]
+        return self.mlp(edge_features)  # shape [E, F_out]
---
+++ src/models/posegraph.py
@@ -0,0 +1,35 @@
+from scipy.spatial.transform import Rotation
+import numpy as np
+import networkx as nx
+from typing import Type
+
+class ObsNode:
+    # Each node holds an observation
+    def __init__(self, obs):
+        self.obs = obs
+
+class PoseEdge:
+    # Each edge holds a 6DOF transform
+    def __init__(self, pos: Type[np.array], rot: Type[Rotation]):
+        self.pos = pos
+        self.rot = rot
+
+
+class PoseGraph(nx.Graph):
+    id_ctr = 0
+    def __init__(self):
+        # Init world frame
+
+    def add_node(self, node, **attr):
+        node.id = self.id_ctr
+        self.id_ctr += 1
+        return super().add_node(node, attr)
+
+    def add_nodes_from(nodes_for_adding, **attr):
+        for n in nodes_for_adding:
+            n.id = self.id_ctr
+            self.id_ctr += 1
+        return super().add_nodes_from(nodes_for_adding, attr)
+        
+
+
