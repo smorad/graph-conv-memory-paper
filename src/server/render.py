0c897360cfc0eb336527d3f6124fea34ae0bf479
S. Morad <sm2558@cam.ac.uk>
2021-01-21 12:53:27 +0000

    Stack multiple imgs per worker in render server

--- src/server/render.py
+++ src/server/render.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import io
 from flask import Flask, render_template, Response
 from PIL import Image
@@ -12,20 +13,39 @@ def index():
     return render_template('index.html')
 
 
-def get_concat_v(img_paths):
+def concat_v_from_paths(img_paths):
     imgs = [Image.open(p) for p in img_paths] 
     dst = Image.new('RGB', (imgs[0].width, imgs[0].height  * len(imgs)))
     for i in range(len(imgs)):
         dst.paste(imgs[i], (0, imgs[0].height * i))
     return dst
 
+def concat_v_from_imgs(imgs):
+    dst = Image.new('RGB', (imgs[0].width, imgs[0].height  * len(imgs)))
+    for i in range(len(imgs)):
+        dst.paste(imgs[i], (0, imgs[0].height * i))
+    return dst
+
+def concat_h_from_paths(img_paths):
+    imgs = [Image.open(p) for p in img_paths] 
+    dst = Image.new('RGB', (imgs[0].width * len(imgs), max(img.height for img in imgs)))
+    for i in range(len(imgs)):
+        dst.paste(imgs[i], (imgs[0].width * i, 0))
+    return dst
+
 def gen():
     while True:
+        if not os.path.isdir(RENDER_ROOT):
+            continue
         render_dirs = [f.path for f in os.scandir(RENDER_ROOT) if f.is_dir()]
-        img_paths = [f'{d}/out.jpg' for d in render_dirs if os.path.isfile(f'{d}/out.jpg')]
-        cat_img = get_concat_v(img_paths)
+        img_paths = [glob.glob(f'{d}/*.jpg') for d in render_dirs]
+        #img_paths = [f'{d}/out.jpg' for d in render_dirs if os.path.isfile(f'{d}/out.jpg')]
+        # Concatenate images in the same dir (rgb, graph, etc) horizontally
+        # and concatenate different dirs (worker1, worker2, etc) vertically
+        h_imgs = [concat_h_from_paths(d) for d in img_paths]
+        final_img = concat_v_from_imgs(h_imgs)
         buf = io.BytesIO()
-        cat_img.save(buf, format='jpeg')
+        final_img.save(buf, format='jpeg')
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + buf.getvalue() + b'\r\n')
 
