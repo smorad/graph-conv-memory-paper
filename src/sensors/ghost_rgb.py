0829f9a11dce8ab8479147fb459ad1637b904534
S. Morad <sm2558@cam.ac.uk>
2021-01-27 14:04:56 +0000

    Add ghost RGB sensor

---
+++ src/sensors/ghost_rgb.py
@@ -0,0 +1,18 @@
+from habitat_baselines.common.obs_transformers import ObservationTransformer
+
+
+class GhostRGB(ObservationTransformer):
+    def __init__(self):
+        super().__init__()
+
+    def transform_observation_space(self, obs_space):
+        obs_space.spaces.pop('rgb', None)
+        return obs_space
+
+    def forward(self, obs):
+        obs.pop('rgb', None)
+        return obs
+
+    def from_config(cls, config):
+        # TODO: Figure out if we need this
+        raise NotImplementedError()
