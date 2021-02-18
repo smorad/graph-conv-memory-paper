edd5c72120e89a40376ac0fe4a38ce18efb0e698
S. Morad <sm2558@cam.ac.uk>
2021-02-18 15:49:00 +0000

    Add AE preprocessor

--- src/models/ae.py
+++ src/models/ae.py
@@ -8,65 +8,35 @@ from torch import nn
 
 
 class CNNAutoEncoder(torch.nn.Module):
-    def __init__(self, num_cats=42):
+    def __init__(self, num_cats=44):
         super().__init__()
         self.num_cats = num_cats
-        self.encoder = CNNEncoder()
-        self.decoder = CNNDecoder()
-
-    def forward(self, x):
-        return self.decoder(self.encoder(x))
-
-    def loss(self, output, target):
-        # Our input is 80 semantic layers and 1 depth layer
-        # let's weight s.t. semantic and depth observations
-        # are equal importance
-        depth_e = torch.mean(output[:, 0] - target[:, 0]) ** 2
-        semantic_e = torch.mean(output[:, 1:] - target[:, 1:] / self.num_cats) ** 2
-        loss = depth_e + semantic_e
-        return loss
-
-
-class CNNEncoder(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
+        self.h_dim = 2048
         # Takes 320x320
         self.encoder = nn.Sequential(
-            nn.Conv2d(43 + 1, 64, 5, stride=3),  # b, 64, 10
+            nn.Conv2d(43 + 1, 128, 5, stride=3),  # b, 64, 10
             nn.ReLU(),
-            nn.Conv2d(64, 96, 4, stride=2),  # b, 96, 4, 4
+            nn.Conv2d(128, 256, 4, stride=2),  # b, 96, 4, 4
             nn.ReLU(),
-            nn.Conv2d(96, 128, 2, stride=2),  # b, 128, 2, 2
+            nn.Conv2d(256, 512, 3, stride=1),  # b, 128, 2, 2
             nn.ReLU(),
+            nn.Flatten(),
         )
-
-    def forward(self, x):
-        """
-        for layer in self.encoder:
-            x = layer(x)
-            print(x.shape)
-        return x
-        """
-        return self.encoder(x)
-
-
-class CNNDecoder(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 96, 2, stride=2),  # b, 128, 2, 2
+            nn.Unflatten(1, (self.h_dim // 4, 2, 2)),  # TODO: This should be variable
+            nn.ConvTranspose2d(512, 256, 3, stride=1),  # b, 128, 2, 2
             nn.ReLU(),
-            nn.ConvTranspose2d(96, 64, 4, stride=2),  # b, 96, 4, 4
+            nn.ConvTranspose2d(256, 128, 4, stride=2),  # b, 96, 4, 4
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 43 + 1, 5, stride=3),  # b, 64, 10
+            nn.ConvTranspose2d(128, 43 + 1, 5, stride=3),  # b, 64, 10
             nn.Sigmoid(),
         )
 
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, z):
+        return self.decoder(z)
+
     def forward(self, x):
-        """
-        for layer in self.decoder:
-            x = layer(x)
-            print(x.shape)
-        return x
-        """
-        return self.decoder(x)
+        return self.decoder(self.encoder(x))
