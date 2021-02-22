f7266724fc642b0b68b078b4a4fbdf0b8cd17da0
S. Morad <sm2558@cam.ac.uk>
2021-02-22 12:16:54 +0000

    Restore old quantization size

--- src/preprocessors/quantized_depth.py
+++ src/preprocessors/quantized_depth.py
@@ -6,7 +6,7 @@ from habitat_baselines.common.obs_transformers import ObservationTransformer
 
 
 class QuantizedDepth(ObservationTransformer):
-    def __init__(self, env, height_fac=32, width_fac=32):
+    def __init__(self, env, height_fac=10, width_fac=10):
         self.env = env
         self.dtype = np.float32
         self.facs = np.array((height_fac, width_fac), dtype=np.int32)
