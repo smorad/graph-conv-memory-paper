e7039760b761d79adbaed3ce5e4cec54edee17ad
S. Morad <sm2558@cam.ac.uk>
2021-03-10 09:27:17 +0000

    Add 1024 dim model to ae

--- src/models/ae.py
+++ src/models/ae.py
@@ -8,29 +8,49 @@ from torch import nn
 
 
 class CNNAutoEncoder(torch.nn.Module):
-    def __init__(self, num_cats=44):
+    def __init__(self, h_dim=2048):
+        assert h_dim in [1024, 2048]
         super().__init__()
-        self.num_cats = num_cats
-        self.h_dim = 2048
-        # Takes 320x320
-        self.encoder = nn.Sequential(
-            nn.Conv2d(43 + 1, 128, 5, stride=3),  # b, 64, 10
-            nn.ReLU(),
-            nn.Conv2d(128, 256, 4, stride=2),  # b, 96, 4, 4
-            nn.ReLU(),
-            nn.Conv2d(256, 512, 3, stride=1),  # b, 128, 2, 2
-            nn.ReLU(),
-            nn.Flatten(),
-        )
-        self.decoder = nn.Sequential(
-            nn.Unflatten(1, (self.h_dim // 4, 2, 2)),  # TODO: This should be variable
-            nn.ConvTranspose2d(512, 256, 3, stride=1),  # b, 128, 2, 2
-            nn.ReLU(),
-            nn.ConvTranspose2d(256, 128, 4, stride=2),  # b, 96, 4, 4
-            nn.ReLU(),
-            nn.ConvTranspose2d(128, 43 + 1, 5, stride=3),  # b, 64, 10
-            nn.Sigmoid(),
-        )
+        self.h_dim = h_dim
+        # Takes 32x32x44
+        if h_dim == 2048:
+            self.encoder = nn.Sequential(
+                nn.Conv2d(43 + 1, 128, 5, stride=3),  # b, 64, 10
+                nn.ReLU(),
+                nn.Conv2d(128, 256, 4, stride=2),  # b, 96, 4, 4
+                nn.ReLU(),
+                nn.Conv2d(256, 512, 3, stride=1),  # b, 128, 2, 2
+                nn.ReLU(),
+                nn.Flatten(),
+            )
+            self.decoder = nn.Sequential(
+                nn.Unflatten(1, (self.h_dim // 4, 2, 2)),
+                nn.ConvTranspose2d(512, 256, 3, stride=1),  # b, 128, 2, 2
+                nn.ReLU(),
+                nn.ConvTranspose2d(256, 128, 4, stride=2),  # b, 96, 4, 4
+                nn.ReLU(),
+                nn.ConvTranspose2d(128, 43 + 1, 5, stride=3),  # b, 64, 10
+                nn.Sigmoid(),
+            )
+        elif h_dim == 1024:
+            self.encoder = nn.Sequential(
+                nn.Conv2d(43 + 1, 128, 5, stride=3),  # b, 64, 10
+                nn.ReLU(),
+                nn.Conv2d(128, 196, 4, stride=2),  # b, 96, 4, 4
+                nn.ReLU(),
+                nn.Conv2d(196, 256, 3, stride=1),  # b, 128, 2, 2
+                nn.ReLU(),
+                nn.Flatten(),
+            )
+            self.decoder = nn.Sequential(
+                nn.Unflatten(1, (self.h_dim // 4, 2, 2)),
+                nn.ConvTranspose2d(256, 196, 3, stride=1),  # b, 128, 2, 2
+                nn.ReLU(),
+                nn.ConvTranspose2d(196, 128, 4, stride=2),  # b, 96, 4, 4
+                nn.ReLU(),
+                nn.ConvTranspose2d(128, 43 + 1, 5, stride=3),  # b, 64, 10
+                nn.Sigmoid(),
+            )
 
     def encode(self, x):
         return self.encoder(x)
