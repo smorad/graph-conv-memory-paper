a0ec83d60af9dd1b48720cb575ded3d8e4b96e65
S. Morad <sm2558@cam.ac.uk>
2021-03-17 19:59:18 +0000

    For some reason, recall remote using graph is nearly as fast as lstm during backprop

--- src/cfg/recall_base.py
+++ src/cfg/recall_base.py
@@ -3,22 +3,53 @@ from ray.tune import register_env
 from recall_env import RecallEnv
 from ray.rllib.agents.impala import ImpalaTrainer
 from models.ray_graph import RayObsGraph
+from ray.tune import grid_search
+
+from models.edge_selectors.bernoulli import BernoulliEdge
+from models.edge_selectors.temporal import TemporalBackedge
 
 
 register_env(RecallEnv.__name__, RecallEnv)
 
-
-model = {
+base_model = {
     "custom_model": RayObsGraph,
     "custom_model_config": {
-        "graph_size": 16,
+        "graph_size": 17,
         "gcn_output_size": 16,
         "gcn_hidden_size": 16,
-        "gcn_num_layers": 3,
-        "edge_selectors": ["learned"],
+        "gcn_num_layers": 8,
+        "edge_selectors": [],
     },
     "max_seq_len": 8,
 }
+
+temporal_model = {
+    "custom_model": RayObsGraph,
+    "custom_model_config": {
+        "graph_size": 17,
+        "gcn_output_size": 16,
+        "gcn_hidden_size": 16,
+        "gcn_num_layers": 8,
+        "edge_selectors": [TemporalBackedge],
+    },
+    "max_seq_len": 8,
+}
+
+bernoulli_model = {
+    "custom_model": RayObsGraph,
+    "custom_model_config": {
+        "graph_size": 17,
+        "gcn_output_size": 16,
+        "gcn_hidden_size": 16,
+        "gcn_num_layers": 8,
+        "edge_selectors": [BernoulliEdge],
+    },
+    "max_seq_len": 8,
+}
+
+rnn_model = {"use_lstm": True, "max_seq_len": 8, "lstm_cell_size": 16}
+
+models = [rnn_model, base_model, temporal_model, bernoulli_model]
 
 CFG = {
     # Our specific trainer type
@@ -28,10 +59,10 @@ CFG = {
         "env_config": {"dim": 4, "max_items": 4, "max_queries": 4},
         # These are rllib/ray specific
         "framework": "torch",
-        "model": model,
-        "num_workers": 1,
+        "model": grid_search(models),
+        "num_workers": 2,
         # Total GPU usage: num_gpus (trainer proc) + num_gpus_per_worker (workers)
-        "num_cpus_per_worker": 1,
+        "num_cpus_per_worker": 4,
         "num_envs_per_worker": 1,
         # this corresponds to the number of learner GPUs used,
         # not the total used for the environments/rollouts
@@ -39,23 +70,13 @@ CFG = {
         # Size of batches (in timesteps) placed in the learner queue
         "rollout_fragment_length": 16,
         # Total number of timesteps to train per batch
-        "train_batch_size": 64,
-        # Vtrace seems to use a ton of GPU memory, and also appears to leak it when using
-        # graph models
-        # In the paper, it does not seem to make a huge performance difference
-        # unless experience replay is used
-        # "vtrace": False,
+        "train_batch_size": 512,
         "lr": 0.0001,
         "env": RecallEnv.__name__,
-        # For evaluation
-        # How many epochs/train iters
-        # "evaluation_interval": 10,
-        # "evaluation_num_episodes": 10,
-        #
-        # "evaluation_config": val_env_cfg,
     },
     "tune": {
         "goal_metric": {"metric": "episode_reward_mean", "mode": "max"},
+        "stop": {"training_iteration": 250},
     },
     # Env to be loaded when mode == human
     "human_env": RecallEnv,
@@ -63,7 +84,10 @@ CFG = {
 
 if os.environ.get("DEBUG", False):
     print("-------DEBUG MODE---------")
+    CFG["ray"]["model"]["custom_model_config"]["edge_selectors"] = [TemporalBackedge]
+    CFG["ray"]["model"]["custom_model_config"]["export_gradients"] = True
+    # CFG['ray']['model']['custom_model_config']
     # CFG['ray']['num_envs_per_worker'] = 1
-    CFG["ray"]["num_workers"] = 0
+    # CFG["ray"]["num_workers"] = 0
     # CFG['ray']['train_batch_size'] = 64
     # CFG['ray']['rollout_fragment_length'] = 32
