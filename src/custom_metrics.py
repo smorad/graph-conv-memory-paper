3967a7a469312377ddbc98ca944f251939fe2531
S. Morad <sm2558@cam.ac.uk>
2021-03-26 11:04:37 +0000

    Use visdom

--- src/custom_metrics.py
+++ src/custom_metrics.py
@@ -1,5 +1,6 @@
 from ray.rllib.agents.callbacks import DefaultCallbacks
 import ray
+import visdom
 
 
 class CustomMetrics(DefaultCallbacks):
@@ -8,10 +9,28 @@ class CustomMetrics(DefaultCallbacks):
 
     def on_episode_end(self, worker, base_env, policies, episode, env_index, **kwargs):
         info = episode.last_info_for()
-        episode.custom_metrics.update({k: info[k] for k in self.INFO_METRICS})
+        episode.custom_metrics.update(
+            {k: info[k] for k in self.INFO_METRICS if k in info}
+        )
+
+
+"""
+class GraphMetrics(DefaultCallbacks, CustomMetrics):
+    def on_train_result(self, *, trainer, result, **kwargs) -> None:
+        m = trainer.get_policy().model
+        metrics = {
+            "graph_density": m.density,
+        }
+        result["custom_metrics"].update(metrics)
+"""
 
 
 class VAEMetrics(DefaultCallbacks):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.visdom = visdom.Visdom("http://localhost", port=5000)
+        self.window_map = {}
+
     def on_train_result(self, *, trainer, result, **kwargs) -> None:
         m = trainer.get_policy().model
         losses = {
@@ -21,6 +40,14 @@ class VAEMetrics(DefaultCallbacks):
             "ae_combined_loss": m.combined_loss.detach().item(),
         }
         result["custom_metrics"].update(losses)
+        for k, imgs in m.visdom_imgs.items():
+            if k not in self.window_map:
+                win_hash = self.visdom.images(imgs, opts={"caption": k, "title": k})
+                self.window_map[k] = win_hash
+            else:
+                self.visdom.images(
+                    imgs, opts={"caption": k, "title": k}, win=self.window_map[k]
+                )
 
 
 class AEMetrics(DefaultCallbacks):
