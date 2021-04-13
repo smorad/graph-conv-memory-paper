8e5769a587d673b9f40c850594920bb199d4515f
Steven Morad <sm2558@cam.ac.uk>
2021-04-13 08:49:41 +0000

    Fix Bernoulli double sample

--- src/models/edge_selectors/bernoulli.py
+++ src/models/edge_selectors/bernoulli.py
@@ -9,14 +9,22 @@ class BernoulliEdge(torch.nn.Module):
     """Add temporal bidirectional back edge, but only if we have >1 nodes
     E.g., node_{t} <-> node_{t-1}"""
 
-    def __init__(self, model: torch.nn.Sequential = None):
+    def __init__(
+        self,
+        input_size: int = 0,
+        model: torch.nn.Sequential = None,
+        clamp_range=[0.001, 0.999],
+    ):
         super().__init__()
+        self.density = torch.tensor(0)
+        self.clamp_range = clamp_range
+        assert input_size or model, "Must specify either input_size or model"
         if model:
             self.edge_network = model
         else:
-            self.edge_network = None
-        self.metrics: Dict[str, np.ndarray] = {}
-        self.density = 0
+            # This MUST be done here
+            # if done in forward model does not learn...
+            self.edge_network = self.build_edge_network(input_size)
 
     def sample_random_var(self, p: torch.Tensor) -> torch.Tensor:
         """Given a probability [0,1] p, return a backprop-capable random sample"""
@@ -25,11 +33,18 @@ class BernoulliEdge(torch.nn.Module):
             torch.log(e) - torch.log(1 - e) + torch.log(p) - torch.log(1 - p)
         )
 
-    def to_hard(self, x: torch.Tensor) -> torch.Tensor:
-        """Hard trick similar to that used in gumbel softmax in torch.
-        Normally, argmax is not differentiable.
-        Rounds to {0, 1} in a differentiable fashion"""
-        res = torch.stack((1.0 - x, x), dim=0).clamp(0.0001, 0.9999)
+    def sample_hard(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Randomly sample a Bernoulli distribution and return the argmax.
+        E.g. sample_hard(torch.tensor([0.4, 0.99, 0]) will likely return
+        either [0, 1, 0] or [1, 1, 0]. Note that the inputs are expected to
+        be probabilities and are clamped to self.clamp_range for numerical
+        stability.
+
+        This uses the gumbel_softmax trick to make argmax differentiable
+        """
+
+        res = torch.stack((1.0 - x, x), dim=0).clamp(*self.clamp_range)
         return torch.nn.functional.gumbel_softmax(torch.log(res), hard=True, dim=0)[1]
 
     def build_edge_network(self, input_size: int) -> torch.nn.Sequential:
@@ -44,6 +59,35 @@ class BernoulliEdge(torch.nn.Module):
             torch.nn.Sigmoid(),
         )
 
+    def detach_loss(self):
+        """Returns a copy of the accumulated loss and resets the loss
+        and associated gradient to zero. Call this exactly once
+        per each backward pass."""
+        loss = self.density.clone()
+        self.density = torch.tensor(0)
+        return loss
+
+    def compute_full_loss(self, nodes: torch.Tensor, B: int):
+        """Compute loss over the entire weight node matrix.
+        (B, N, feat) by (B, N*N, feat)"""
+        N = nodes.shape[1]
+        feat = nodes.shape[-1]
+        # Right: from [[0, 1, 2], [3,4,5]]
+        # to [[0,1,2], [3,4,5], [0,1,2], [3,4,5] ... ]
+        left_nodes = nodes.repeat_interleave(N, dim=1)
+        # Left: from [[0, 1, 2], [3,4,5]]
+        # to [[0,1,2], [0,1,2]... [3,4,5], [3,4,5]]
+        right_nodes = nodes.repeat(1, N, 1)
+        edge_net_in = torch.cat((left_nodes, right_nodes), dim=-1)
+        # Free memory
+        del left_nodes
+        del right_nodes
+        # Edge network expects [B, feat] but we have [B, N, feat]
+        # so flatten to [B, feat] for big perf gainz and unflatten
+        batch_in = edge_net_in.view(B * N ** 2, 2 * feat)
+        batch_out = self.edge_network(batch_in)
+        return batch_out.mean()
+
     def compute_logits(
         self,
         nodes: torch.Tensor,
@@ -56,7 +100,7 @@ class BernoulliEdge(torch.nn.Module):
 
         # No edges for a single node
         if torch.max(num_nodes) == 0:
-            return weights.clamp(0.0001, 0.9999)
+            return weights.clamp(*self.clamp_range)
 
         B_idx = torch.arange(B)
         n = torch.max(num_nodes)
@@ -70,7 +114,9 @@ class BernoulliEdge(torch.nn.Module):
         # so flatten to [B, feat] for big perf gainz and unflatten
         batch_in = edge_net_in.view(B * n, 2 * feat)
         batch_out = self.edge_network(batch_in)
-        probs = batch_out.view(B, n).clamp(0.0001, 0.9999)
+        probs = batch_out.view(B, n).clamp(*self.clamp_range)
+        # print(probs.mean())
+        self.density = self.density + probs.mean()  # probs.sum() / num_nodes.sum()
 
         # Undirected edges
         # TODO: This is not equivariant as nn(a,b) != nn(b,a), run both dirs thru
@@ -82,7 +128,8 @@ class BernoulliEdge(torch.nn.Module):
             # This does NOT add self edge [num_nodes[b], num_nodes[b]]
             weights[b, num_nodes[b], : num_nodes[b]] = probs[b][: num_nodes[b]]
             weights[b, : num_nodes[b], num_nodes[b]] = probs[b][: num_nodes[b]]
-            self.density = self.density + probs[b][: num_nodes[b]].mean()
+            # if num_nodes[b] > 0:
+            #    self.density = self.density + probs[b][: num_nodes[b]].mean()
 
         # Nx2 possible rows
         # self.density = self.density + 2 * batch_out.sum() / ((num_nodes + 1) * 2).sum()
@@ -99,14 +146,12 @@ class BernoulliEdge(torch.nn.Module):
 
         # a(b,i,j) = gumbel_softmax(phi(n(b,i) || n(b,j))) for i, j < num_nodes
         # First run
-        if not self.edge_network:
-            self.edge_network = self.build_edge_network(nodes.shape[-1])
         if self.edge_network[0].weight.device != nodes.device:
             self.edge_network = self.edge_network.to(nodes.device)
 
         # Weights serve as probabilities that we sample from
         weights = self.compute_logits(nodes, num_nodes, weights, B)
-        sample = self.sample_random_var(weights)
-        adj = self.to_hard(sample)
+        # sample = self.sample_random_var(weights)
+        adj = self.sample_hard(weights)
 
         return adj, weights
